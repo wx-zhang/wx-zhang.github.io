e875d0d54d9486bc8724702eeba72d7aa884a79d
Wenxuan Zhang <49470176+wx-zhang@users.noreply.github.com>
2023-12-10 18:44:48 +0300

    Updated HTML.

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+
+source $(brew --prefix)/opt/chruby/share/chruby/chruby.sh
+source $(brew --prefix)/opt/chruby/share/chruby/auto.sh
+chruby ruby-3.1.3
+bundle install
 bundle add webrick
 
 bundle exec jekyll server
